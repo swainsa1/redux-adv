19020e7ad762c4a7a680d555767495ee8f442c74
swainsa1 <santosh.swain@gmail.com>
2021-01-11 17:52:41 -0600

    Async function apprach with thunk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk'
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -13,8 +14,17 @@ const rootReducer = combineReducers({
   ctr: counterReducer,
   res: resultReducer,
 });
-
-const store = createStore(rootReducer);
+const logger = store => {
+  return next => {
+      return action => {
+        console.log('[MiddleWare] -- Dispatching',action);
+        const result = next(action);
+        console.log('[MiddleWare] Next STate update',store.getState());
+        return result;
+      }
+  }
+};
+const store = createStore(rootReducer,applyMiddleware(logger,thunk));
 ReactDOM.render(
   <Provider store={store}>
     <App />
